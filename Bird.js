b0ea8e79d1e3211831543be3397961d6de8a1684
ebdevStudent <78286626+ebdevStudent@users.noreply.github.com>
2021-08-13 13:15:06 -0400

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,25 @@
+class Bird{
+    constructor(x, y){
+        var options = {
+            'restitution': 0.8,
+            'friction': 1.0,
+            'density': 1.0
+        }
+        this.body = Bodies.rectangle(x, y, 50, 50, options);
+        this.image = loadImage("sprites/bird.png");
+        this.width = 50;
+        this.height = 50;
+        World.add(world, this.body);
+    }
+    display(){
+        this.body.position.x = mouseX;
+        this.body.position.y = mouseY;
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop();
+    }
+}
